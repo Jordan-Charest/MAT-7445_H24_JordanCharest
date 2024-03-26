33bda14cbb5bd96bbcee737639c2cb11be14e614
Jordan-Charest <jordan.charest.2@ulaval.ca>
2024-03-26 14:13:23 -0400

    Commentaires

--- HH_anim.py
+++ HH_anim.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.integrate import solve_ivp
-# from eqs_HH import *
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
@@ -19,6 +18,8 @@ E_L = -65
 g_L = 0.3
 C = 1
 
+# Équations auxiliaires
+
 def alpha_n(V):
     return 0.02 * (V - 25) / (1 - np.exp(-(V-25)/9))
 
@@ -36,6 +37,8 @@ def alpha_h(V):
 
 def beta_h(V):
     return 0.25 * np.exp((V+62)/6) / np.exp((V+90)/12)
+
+# Équations différentielles
 
 def CdV_dt(V, m, n, h, I_ext):
     return g_L*(E_L-V) + g_Na*(m**3)*h*(E_Na-V) + g_K*(n**4)*(E_K-V) + I_ext
@@ -71,7 +74,6 @@ def solve(I_ext):
     return sol
 
 
-# Solve for all I_ext values
 V_min = 1000
 V_max = -1000
 n_min = 1000
@@ -103,25 +105,25 @@ n_max = n_max*1.1
 
 fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
-# Initialize empty plots
+
 line_V, = axs[0].plot([], [], label='V', color='blue')
 line_m, = axs[1].plot([], [], label='m', color='red')
 line_n, = axs[1].plot([], [], label='n', color='green')
 line_h, = axs[1].plot([], [], label='h', color='orange')
 
-# Plotting V_data vs. t_data
+# V vs t
 axs[0].set_title('V as a function of t')
 axs[0].set_xlabel('t')
 axs[0].set_ylabel('V')
 axs[0].legend()
 
-# Plotting m_data, n_data, and h_data vs. t_data
+# m, n, h vs t
 axs[1].set_title('m, n and h vs. t')
 axs[1].set_xlabel('t')
 axs[1].set_ylabel('Values')
 axs[1].legend()
 
-# Update function for animation
+# Animation
 def update(frame):
     sol = solns[frame]
     V_data, m_data, n_data, h_data = sol.y
@@ -134,14 +136,11 @@ def update(frame):
     axs[0].set_xlim(0, t_final)
     axs[1].set_xlim(0, t_final)
     axs[0].set_ylim(V_min, V_max)
-    # axs[0].set_ylim(0, 5)
     axs[1].set_ylim(n_min, n_max)
     return line_V, line_m, line_n, line_h
 
 # Set animation
 ani = FuncAnimation(fig, update, frames=len(solns), blit=False, interval=100)
-
-
 
 # Adjust layout
 plt.tight_layout()
