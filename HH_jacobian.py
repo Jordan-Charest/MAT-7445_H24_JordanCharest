Added fixed point and jacobian calculations

HH_jacobian.py
@@ -1,58 +1,58 @@
-import autograd.numpy as np
+import jax.numpy as jnp
+import jax
 from autograd import grad, jacobian
 from numpy.linalg import eig
+from HH_eqs import *
+from scipy.optimize import root
 
-E_Na = 55.0
-g_Na = 40.0
-E_K = -77.0
-g_K = 35.0
-E_L = -65.0
-g_L = 0.3
-C = 1.0
-I_ext = 0.375
+def eqs(y):
+    
+    V, m, n, h = y
 
-def alpha_n(V):
-    return 0.02 * (V - 25.0) / (1.0 - np.exp(-(V-25.0)/9.0))
+    return [
+        CdV_dt(V, m, n, h, I_ext),
+        dm_dt(V, m, n, h, I_ext),
+        dn_dt(V, m, n, h, I_ext),
+        dh_dt(V, m, n, h, I_ext)
+    ]
 
-def beta_n(V):
-    return -0.002 * (V - 25.0) / (1.0 - np.exp((V-25.0)/9.0))
+# Nous devons d'abord déterminer les points fixes du système pour différentes valeurs de I_ext
 
-def alpha_m(V):
-    return 0.182 * (V + 35.0) / (1.0 - np.exp(-(V+35.0)/9.0))
+# I_ext_list = [0, 0.37, 0.375, 0.5, 1, 3, 10]
+I_ext_list = jnp.arange(0.2, 0.46, 0.02)
+x0 = [-50, 0, 0, 0]
+args = ()
 
-def beta_m(V):
-    return -0.124 * (V + 35.0) / (1.0 - np.exp((V+35.0)/9.0))
+fixed_points = []
+success_list = []
 
-def alpha_h(V):
-    return 0.25 * np.exp(-(V+90.0)/12.0)
+for I in I_ext_list:
+    I_ext = I
+    sol = root(eqs, x0, args)
+    fixed_points.append(sol.x)
+    success_list.append(sol.success)
 
-def beta_h(V):
-    return 0.25 * np.exp((V+62.0)/6.0) / np.exp((V+90.0)/12.0)
+for i in range(len(fixed_points)):
+    print(f"Succès: {success_list[i]}")
+    print(f"Point fixe pour I_ext = {I_ext_list[i]:.2f}:")
+    print(fixed_points[i])
 
-def eqs(x):
+print("\n")
 
-    V, m, n, h = x
+Jacobian = jax.jacobian(eqs)
 
-    return np.array([
-        g_L*(E_L-V) + g_Na*(m**3.0)*h*(E_Na-V) + g_K*(n**4.0)*(E_K-V) + I_ext,
-        alpha_m(V)*(1-m) - beta_m(V)*m,
-        alpha_n(V)*(1-n) - beta_n(V)*n,
-        alpha_h(V)*(1-h) - beta_h(V)*h
-    ])
+for i in range(len(fixed_points)):
 
-Jacobian = jacobian(eqs)
+    x = jnp.asarray(fixed_points[i])
 
-# Define the point at which to compute the Jacobian
-# x = np.array([-60.09034538949499, 0.08286126628477544, 0.0007827628374709542, 0.42109601367748734])
-x = np.array([-58.04926217066852, 0.10159214179608508, 0.0009461804432894837, 0.3765203020172575])
+    # Evaluate the Jacobian matrix at the given point
+    result = Jacobian(x)
 
-# Evaluate the Jacobian matrix at the given point
-result = Jacobian(x)
+    print(f"Jacobien à I_ext = {I_ext_list[i]:.2f}:")
+    print(jnp.asarray(result))
 
-print("Jacobien:")
-print(result)
+    eigenvalues = eig(result)[0]
 
-eigenvalues = eig(result)[0]
-
-print("Valeurs propres:")
-print(eigenvalues)
+    print("Valeurs propres:")
+    print(eigenvalues)
+    print("\n")

HH_plot.py
@@ -1,65 +1,14 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-
-# Constantes
-E_Na = 55
-g_Na = 40
-E_K = -77
-g_K = 35
-E_L = -65
-g_L = 0.3
-C = 1
-
-# Équations auxiliaires
-def alpha_n(V):
-    return 0.02 * (V - 25) / (1 - np.exp(-(V-25)/9))
-
-def beta_n(V):
-    return -0.002 * (V - 25) / (1 - np.exp((V-25)/9))
-
-def alpha_m(V):
-    return 0.182 * (V + 35) / (1 - np.exp(-(V+35)/9))
-
-def beta_m(V):
-    return -0.124 * (V + 35) / (1 - np.exp((V+35)/9))
-
-def alpha_h(V):
-    return 0.25 * np.exp(-(V+90)/12)
-
-def beta_h(V):
-    return 0.25 * np.exp((V+62)/6) / np.exp((V+90)/12)
-
-# Équations différentielles
-
-def CdV_dt(V, m, n, h, I_ext):
-    return g_L*(E_L-V) + g_Na*(m**3)*h*(E_Na-V) + g_K*(n**4)*(E_K-V) + I_ext
-
-def dm_dt(V, m, n, h, I_ext):
-    return alpha_m(V)*(1-m) - beta_m(V)*m
-
-def dn_dt(V, m, n, h, I_ext):
-    return alpha_n(V)*(1-n) - beta_n(V)*n
-
-def dh_dt(V, m, n, h, I_ext):
-    return alpha_h(V)*(1-h) - beta_h(V)*h
-
-def eqs(t, y, I_ext):
-
-    V, m, n, h = y
-
-    return [
-        CdV_dt(V, m, n, h, I_ext),
-        dm_dt(V, m, n, h, I_ext),
-        dn_dt(V, m, n, h, I_ext),
-        dh_dt(V, m, n, h, I_ext)
-    ]
+from HH_eqs import *
 
 # Intégrer les équations
 
 t_final = 500
-I_ext = 0
+I_ext = 0.375
 initial = [-60, 0, 0, 0]
+# initial = [-60, 0.09655771, 0.02410834, 0.07001662]
 
 integrator_keywords = {}
 integrator_keywords["rtol"] = 1e-12
@@ -70,6 +19,13 @@ sol = solve_ivp(eqs, (0, t_final), initial, args=(I_ext,), **integrator_keywords
 
 t_data = sol.t
 (V_data, m_data, n_data, h_data) = sol.y
+
+# On peut décommenter cette ligne pour observer les dernières valeurs des paramètres, p. ex pour voir s'il y a un pt fixe
+# print(f"n_timesteps: {len(V_data)}")
+# print(f"Final V: {V_data[-10:]}")
+# print(f"Final m: {m_data[-10:]}")
+# print(f"Final n: {n_data[-10:]}")
+# print(f"Final h: {h_data[-10:]}")
 
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))
@@ -103,5 +59,5 @@ axs[1, 1].set_xlabel('h')
 axs[1, 1].set_ylabel('m')
 
 plt.subplots_adjust(hspace=0.3)
-# plt.savefig(f"{path}/Iext={I_ext}.png")
+# plt.savefig(f"{path}/Iext={I_ext}_long.png")
 plt.show()