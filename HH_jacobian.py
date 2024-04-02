3cedc3f432fab380557414954520a1fe90195832
Jordan-Charest <jordan.charest.2@ulaval.ca>
2024-04-02 10:55:26 -0400

    Update HH_jacobian.py

--- HH_jacobian.py
+++ HH_jacobian.py
@@ -1,9 +1,9 @@
 import jax.numpy as jnp
 import jax
-from autograd import grad, jacobian
 from numpy.linalg import eig
 from HH_eqs import *
 from scipy.optimize import root
+import matplotlib.pyplot as plt
 
 def eqs(y):
     
@@ -19,8 +19,8 @@ def eqs(y):
 # Nous devons d'abord déterminer les points fixes du système pour différentes valeurs de I_ext
 
 # I_ext_list = [0, 0.37, 0.375, 0.5, 1, 3, 10]
-I_ext_list = jnp.arange(0.2, 0.46, 0.02)
-x0 = [-50, 0, 0, 0]
+I_ext_list = jnp.arange(0.2, 0.52, 0.005)
+x0 = [-60, 0, 0, 0]
 args = ()
 
 fixed_points = []
@@ -34,12 +34,14 @@ for I in I_ext_list:
 
 for i in range(len(fixed_points)):
     print(f"Succès: {success_list[i]}")
-    print(f"Point fixe pour I_ext = {I_ext_list[i]:.2f}:")
+    print(f"Point fixe pour I_ext = {I_ext_list[i]:.3f}:")
     print(fixed_points[i])
 
 print("\n")
 
 Jacobian = jax.jacobian(eqs)
+
+eigenvals = []
 
 for i in range(len(fixed_points)):
 
@@ -48,11 +50,41 @@ for i in range(len(fixed_points)):
     # Evaluate the Jacobian matrix at the given point
     result = Jacobian(x)
 
-    print(f"Jacobien à I_ext = {I_ext_list[i]:.2f}:")
+    print(f"Jacobien à I_ext = {I_ext_list[i]:.3f}:")
     print(jnp.asarray(result))
 
     eigenvalues = eig(result)[0]
+    eigenvals.append(eigenvalues)
 
     print("Valeurs propres:")
     print(eigenvalues)
     print("\n")
+
+split_eig = ([], [], [], [])
+for i in range(len(eigenvals)):
+    for j in range(4):
+        split_eig[j].append(eigenvals[i][j])
+
+fig, ax = plt.subplots()
+
+bif = np.argmin(np.abs(np.asarray(split_eig[1])-np.asarray(split_eig[2])))
+
+ax.plot(I_ext_list, split_eig[1], label="Valeur propre 2")
+ax.plot(I_ext_list, split_eig[2], label="Valeur propre 3")
+ax.plot(I_ext_list, split_eig[3], color="red", label="Valeur propre 4")
+ax.hlines(0, 0.2, 0.5, color="grey", linestyles="dashed")
+ax.vlines(0.4, -0.2, 0.2, color="grey", linestyles="dotted")
+ax.vlines(I_ext_list[bif], -0.2, 0.2, color="grey", linestyles="dotted")
+ax.set_title("Partie réelle des valeurs propres 2, 3 et 4 en fonction de I_ext")
+ax.legend()
+
+plt.show()
+plt.clf()
+
+fig, ax = plt.subplots()
+
+ax.plot(I_ext_list, split_eig[0], color="blue", label="Valeur propre 1")
+ax.legend()
+ax.set_title("Partie réelle de la valeur propre 1 en fonction de I_ext")
+
+plt.show()
